d1239b16d7715584598c7876e0cb036e52d98e33
Kaleb Hedrick <destroycows@gmail.com>
2023-10-13 15:36:20 -0400

    Authentication file

---
+++ api/authentication.js
@@ -0,0 +1,14 @@
+function authenticate(Username, Password) { //return true if user was found, false otherwise
+    return null;
+}
+function createUser (Username, Password) { //creates a new user with the given credentials
+    return null;
+}
+function addNewUserData (Username, Password) { //add initial user data given at account creation
+    return null;
+}
+
+
+
+
+//Tests for user authentication functions
