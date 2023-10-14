ef8e9b21597d7fde68fe4c020baebd94fbce9447
Joshua Kanter <jhkanter@gmail.com>
2023-10-14 16:45:42 -0400

    Moved functions to limits file

--- api/expense.js
+++ api/expense.js
@@ -107,26 +107,3 @@ export function getExpensesCount(userId){
     return null;
 }
 
-/**
- * Get the monthly limit for a user
- * 
- * @param {string} userId the unique ID of the user
- * @returns a float number that shows what the monthly limit for the user is
- */
-export function getMonthlyLimit(userId){
-    //import income class data
-    return null;
-}
-
-/**
- * Calculate how much a user can spend daily until the specified End date
- * 
- * @param {string} userID 
- * @param {Date} endDate 
- * @returns how much the user can spend daily until endDate
- */
-export function calculateDailyMax(userID, endDate)
-{
-    return null;
-}
-
--- api/expenseFunctions.test.js
+++ api/expenseFunctions.test.js
@@ -19,12 +19,3 @@ test("Calling getAllExpenses with null startDate and null endDate returns a coll
 test("Calling getAllExpenses with null startDate and null endDate returns a collection whose earliest item is the same as the output of getEarliestExpense"), () => {
     expect(allExpenses[0].date).toEqual(getEarliestExpense(userId))
 }
-
-test("Calling calculate daily max for 7 days with $700"), () => {
-    //Need to set up a fake user with 700 total
-    let daysToAdd = 7;
-    let currentDate = new Date();
-    let weekLater = currentDate.setDate(currentDate.getDate() + daysToAdd); 
-    let dailyMax = calculateDailyMax(userId, weekLater);
-    expect(dailyMax).toEqual(100);
-}
