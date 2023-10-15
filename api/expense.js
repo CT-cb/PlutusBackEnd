200bc6e78e9bb89eefe78ca66f28031cd8feaf25
iazher <imanazher00@gmail.com>
2023-10-14 20:36:21 -0400

    add/delete expense functions

--- api/expense.js
+++ api/expense.js
@@ -107,3 +107,24 @@ export function getExpensesCount(userId){
     return null;
 }
 
+/**
+ * Add an expense to the list of expenses.
+ *
+ * @param {Object} expenseData - The expense data to be added.
+ * @param {Array} expenses - The array of expenses to which the expenseData will be added.
+ */
+export function addExpense(expenseData, expenses) {
+    // ... (function code)
+  }
+  
+  /**
+   * Delete an expense by ID from the list of expenses.
+   *
+   * @param {number} expenseId - The ID of the expense to be deleted.
+   * @param {Array} expenses - The array of expenses from which the expense will be deleted.
+   * @returns {boolean} Returns true if the expense was deleted, false if the ID was not found.
+   */
+  export function deleteExpense(expenseId, expenses) {
+    // ... (function code)
+  }
+
