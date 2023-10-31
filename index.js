b86b2ed0f0d6f3332dcf1713ed344a6e10f662d2
CT-cb <61859499+CT-cb@users.noreply.github.com>
2023-10-30 22:21:43 -0400

    Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} !!!`));
+app.get('/',function(req,res) {
+    res.json("The API test worked.");
+})
