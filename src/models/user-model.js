fb77fb1193fabf75b03d623619cb047f82787b1d
CT-cb <61859499+CT-cb@users.noreply.github.com>
2023-11-03 06:41:09 -0400

    a

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const sessionSchema = require('session-schema.js').sessionSchema;
 
 const userSchema = new mongoose.Schema({
     userId: {
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     activeSessions: {
-        type: Array,
+        type: sessionSchema, // TODO: this is not the right way to express this!
         required: true
     }
 });
