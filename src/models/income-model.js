9385293574448b37bef6a0b5efe729bfe956e403
iazher <imanazher00@gmail.com>
2023-11-07 21:03:14 -0500

    Income model schema added

--- src/models/income-model.js
+++ src/models/income-model.js
@@ -1,1 +1,47 @@
 const mongoose = require('mongoose');
+
+const incomeSchema = new mongoose.Schema({
+    incomeId: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    incomeDate: {
+        type: Date,
+        required: true
+    },
+    sourceType: {
+        type: String,
+        required: true
+    },
+    source: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        }
+    },
+    seriesId: {
+        type: String
+    }
+});
+
+const Income = mongoose.model('Income', incomeSchema);
+module.exports = Income;
