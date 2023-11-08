101a24d164791083c5d08b22d47356b073ef8179
iazher <imanazher00@gmail.com>
2023-11-07 21:08:59 -0500

    limit schema added

--- src/models/limit-model.js
+++ src/models/limit-model.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+const limitSchema = new mongoose.Schema({
+    limitId: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date
+    },
+    timeDivision: {
+        type: String,
+        required: true
+    },
+    maxLimit: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    }
+});
+
+const Limit = mongoose.model('Limit', limitSchema);
+module.exports = Limit;
+
