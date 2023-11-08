4a504f68240d7b5251add8315bc2038af3df79e4
mrusk2 <mason.m.rusk@gmail.com>
2023-11-08 15:48:48 -0500

    Trying to get dotenv to work

--- src/helpers/mongodb-connect.js
+++ src/helpers/mongodb-connect.js
@@ -7,27 +7,24 @@ const uri = process.env.MONGODB_CONNECTION_URI;
 //Make a variable to utilize mongoose (package for mongodb connections)
 const mongoose = require("mongoose");
 //Connect to the database using .connect
-//mongoose.connect("mongodb://(ip#):(port#)/(database name)");
-mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true})
-    //handle initial errors
-    .catch(error => handleError(error));
+mongoose.connect(uri, 
+    {
+        useNewUrlParser:true, 
+        useUnifiedTopology:true
+    }
+);
 //Make a variable to monitor the status of connection
 const connection = mongoose.connection;
 
 
 //successful case of connection
 connection.once("open", () =>{
-    console.log("successful database connection")
+    console.log("Successful database connection!")
 });
-
-//establish a port connection
-let port = Number("3000");
-var app = express();
-app.listen(port, function(req, res) {
-    console.log("server is started on port " + port)
-});
-
 //handle ongoing errors 
 connection.on("error", err => {
     console.log(err);
 });
+
+
+//establish a port connection...
