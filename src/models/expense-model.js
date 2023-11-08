cbfb28df7ef8e8a70c9ec7894a1faefdedae5964
iazher <imanazher00@gmail.com>
2023-11-07 21:14:47 -0500

    Expense model added

--- src/models/expense-model.js
+++ src/models/expense-model.js
@@ -1,1 +1,50 @@
 const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    transId: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    method: {
+        type: String,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    transDate: {
+        type: Date,
+        required: true
+    },
+    sourceType: {
+        type: String,
+        required: true
+    },
+    payee: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        category: {
+            type: String
+        }
+    },
+    seriesId: {
+        type: String
+    }
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+module.exports = Expense;
