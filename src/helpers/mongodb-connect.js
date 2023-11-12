4e091b2e92536de431575eaacc481fd80103ddbd
mrusk2 <mason.m.rusk@gmail.com>
2023-11-12 14:12:44 -0500

    Update mongodb-connect.js

--- src/helpers/mongodb-connect.js
+++ src/helpers/mongodb-connect.js
@@ -1,5 +1,6 @@
 //Establish environment variables for connection
-const dotenv = require('dotenv');
+require('dotenv').config();
+const express = require('express');
 const password = process.env.MONGODB_CONNECTION_AUTH_URI;
 const uri = process.env.MONGODB_CONNECTION_URI;
 
@@ -7,24 +8,27 @@ const uri = process.env.MONGODB_CONNECTION_URI;
 //Make a variable to utilize mongoose (package for mongodb connections)
 const mongoose = require("mongoose");
 //Connect to the database using .connect
-mongoose.connect(uri, 
-    {
-        useNewUrlParser:true, 
-        useUnifiedTopology:true
-    }
-);
+//mongoose.connect("mongodb://(ip#):(port#)/(database name)");
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true})
+    //handle initial errors
+    .catch(error => console.log(error));
 //Make a variable to monitor the status of connection
 const connection = mongoose.connection;
 
 
 //successful case of connection
 connection.once("open", () =>{
-    console.log("Successful database connection!")
+    console.log("successful database connection")
 });
+
+//establish a port connection
+let port = Number("3000");
+var app = express();
+app.listen(port, function(req, res) {
+    console.log("server is started on port " + port)
+});
+
 //handle ongoing errors 
 connection.on("error", err => {
     console.log(err);
 });
-
-
-//establish a port connection...
