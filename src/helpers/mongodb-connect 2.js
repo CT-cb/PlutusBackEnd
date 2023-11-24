0c13c1c3bbe95ff6f63e4145eff1158372a4ff3d
mrusk2 <mason.m.rusk@gmail.com>
2023-11-24 15:27:16 -0500

    Create mongodb-connect 2.js

---
+++ src/helpers/mongodb-connect 2.js
@@ -0,0 +1,34 @@
+//Establish environment variables for connection
+require('dotenv').config();
+const express = require('express');
+const password = process.env.MONGODB_CONNECTION_AUTH_URI;
+const uri = process.env.MONGODB_CONNECTION_URI;
+
+
+//Make a variable to utilize mongoose (package for mongodb connections)
+const mongoose = require("mongoose");
+//Connect to the database using .connect
+//mongoose.connect("mongodb://(ip#):(port#)/(database name)");
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true})
+    //handle initial errors
+    .catch(error => console.log(error));
+//Make a variable to monitor the status of connection
+const connection = mongoose.connection;
+
+
+//successful case of connection
+connection.once("open", () =>{
+    console.log("successful database connection")
+});
+
+//establish a port connection
+let port = Number("3000");
+var app = express();
+app.listen(port, function(req, res) {
+    console.log("server is started on port " + port)
+});
+
+//handle ongoing errors 
+connection.on("error", err => {
+    console.log(err);
+});
