1436ff960ff60df58741180d1ae8235685b6da5c
iazher <imanazher00@gmail.com>
2023-11-28 23:18:33 -0500

    testing attempt

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PLUTUS_API_PORT;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} !!!`));
 app.get('/',function(req,res) {
     res.json("The API test worked.");
 })
+
+app.use('/incomes', require('./routes/income-route.js'))    
