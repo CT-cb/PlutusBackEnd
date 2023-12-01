e59e95ea1a0a94e33ff4bf50f8053b595becbf14
mrusk2 <mason.m.rusk@gmail.com>
2023-12-01 11:57:59 -0500

    Updated the income tests

--- src/tests/income-tests/add-income.test.js
+++ src/tests/income-tests/add-income.test.js
@@ -1,6 +1,9 @@
-// use the jest framework and establish a timeout
+//Use the jest framework and establish a timeout
 require('jest');
 jest.setTimeout(10000);
+
+//Also use the supertest framework
+const request = require('supertest');
 
 const app = require('../../app.js').app;
 const mongoose = require('mongoose');
@@ -11,43 +14,47 @@ let connection = connectToMongoDb();
 const req = request(app);
 
 
-// connecting to the database
+//Connecting to the database
 beforeAll(() => {
     mongoose.connection.useDb("incomes");
 });
 
-
+//Collection of adding income tests
 describe('/add tests', () => {
 
-
-    //Testing if the transaction date matches
-    test("Should match the transaction date for the correct object", () => {
-        let correctObj = new IncomeModel({
-            "email":"planwithplutus@gmail.com",
-            "amount":300,
-            "method":"credit_card",
-            "type":"vacation",
-            "currency":"usd",
-            "incomeDate":"2021-10-23T21:03:01.522+00:00"
-        });
-
-        
-        expect(correctObj.someFunction()).toBe(someExpectedValue);
-
+    let correctObj = new IncomeModel({
+        "email":"planwithplutus@gmail.com",
+        "amount":1000,
+        "currency":"usd",
+        "incomeDate":"2021-10-23T21:03:01.522+00:00",
+        "type":"direct_deposit"
     });
 
-        let incorrectObj = {
-            "email": "planwithplutus@gmail.com",
-            "currency": "usd",
-            "expenseDate": Date.now()
-        }
+    let incorrectObj = {
+        "email": "planwithplutus@gmail.com",
+        "currency": "usd",
+        "incomeDate": Date.now()
+    }
+
+    //Test that the incorrect object does not get added to the database
+    test('incorrect obj is not added', async () => {
+        let res = await req
+            .post('/incomes/add')
+            .send(JSON.stringify(incorrectObj))
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json');
+
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('status', 'error');
+        expect(res.body).toHaveProperty('errorType');
+        expect(res.body).toHaveProperty('message');
+    });
 
 });
-
-
 
 
 // disconnecting from the database
 afterAll(async () => {
     mongoose.disconnect();
 });
+
