9bd41112fee296f564c30305e8015bf3953f5802
Kaleb Hedrick <destroycows@gmail.com>
2023-12-01 23:08:40 -0500

    added tests

---
+++ src/tests/authCreate.test.js
@@ -0,0 +1,89 @@
+require('jest');
+let badEmail = "plutustestname";
+let goodEmail = "plutustestname@plutus.com"
+let pwd = "abc123"
+const CreateUrl = "http://3.17.169.64:3000/auth/create"
+
+test("create account post did not have proper json email", () => {
+        fetch(CreateUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: badEmail,
+    password: pwd,
+    firstName: "Kaleb",
+    lastName: "Hedrick"
+  })
+    })}).then(response => response.json())
+    .then(json => {
+     return expect(json.status).toEqual("error");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    test("create account post did not have a password", () => {
+        fetch(CreateUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: goodEmail,
+    password: "",
+    firstName: "Kaleb",
+    lastName: "Hedrick"
+  })
+    })}).then(response => response.json())
+    .then(json => {
+     return expect(json.status).toEqual("error");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    test("create account post did not have a first name", () => {
+        fetch(CreateUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: goodEmail,
+    password: pwd,
+    firstName: "",
+    lastName: "Hedrick"
+  })
+    })}).then(response => response.json())
+    .then(json => {
+     return expect(json.status).toEqual("error");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    test("create account post did not have a last name", () => {
+        fetch(CreateUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: goodEmail,
+    password: pwd,
+    firstName: "Kaleb",
+    lastName: ""
+  })
+    })}).then(response => response.json())
+    .then(json => {
+     return expect(json.status).toEqual("error");
+    })
+    .catch(error => {
+      console.error(error);
+    });
---
+++ src/tests/expenses.test.js
@@ -0,0 +1,10 @@
+require('jest');
+let badEmail = "plutustestname";
+let goodEmail = "plutustestname@plutus.com"
+let pwd = "abc123"
+const ExpenseByTypeUrl = "http://3.17.169.64:3000/expenses/bytype"
+
+//expenses by type
+test("Returns proper vacation expenses", () => {
+
+})
